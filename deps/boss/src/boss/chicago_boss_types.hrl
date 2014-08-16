dcbfad889de6cc073ba8adc1e36303baf4287f3a
Elias Mårtenson <lokedhs@gmail.com>
2014-08-16 22:07:32 +0800

    Clean up deps

--- deps/boss/src/boss/chicago_boss_types.hrl
+++
@@ -1,3 +0,0 @@
-%% NO Executable code, type declarations only 
-
--type execution_mode() ::  'development' | 'production'.
--- deps/boss/test/boss_elixir_compiler_test.erl
+++
@@ -1,3 +0,0 @@
--module(boss_elixir_compiler_test).
--include_lib("eunit/include/eunit.hrl").
--include("../src/boss/boss_web.hrl").
